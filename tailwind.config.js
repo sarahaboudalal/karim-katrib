2e893d2740a3283e7ad5942aef21015f2cfabbcf
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 16:20:25 +0200

    Configure: Website colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whitish: '#F3F3F3',
+        beige: '#E9DCBE',
+        grey: '#8E8B82',
+        blackish: '#343434',
+      },
+    },
   },
   plugins: [],
-}
+};

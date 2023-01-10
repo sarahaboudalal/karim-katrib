3644cb2f0e64d699bba2e507ac85595a799cafcc
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 17:45:08 +0200

    adjustment: style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="w-full sm:flex-row flex-col bg-beige items-center flex justify-center sm:p-2 p-10">
-      <div className="text-blackish sm:text-justify text-center sm:p-8  p-3 flex flex-col justify-center">
-        <p className="text-2xl underline font-semibold pb-10">About Me</p>
-        <p className="pb-3 sm:text-xl">
+    <div className="w-full md:flex-row flex-col bg-beige items-center flex justify-center sm:p-2 p-10">
+      <div className="text-blackish text-justify sm:p-8  p-3 flex flex-col justify-center">
+        <p className="text-4xl underline font-semibold pb-10">About Me</p>
+        <p className="pb-3 md:text-xl text-lg">
           Hello! My name is Karim Katrib and I'm a professional fashion model.
         </p>
-        <p p className="pb-3 sm:text-xl">
+        <p p className="pb-3 md:text-xl text-lg">
           I started as an Instagram model, and with consistency and hard work,
           <br className="lg:block hidden" /> I made a lot of succeess in my
           modeling career.
         </p>
-        <p className="sm:text-xl">
+        <p className="md:text-xl text-lg">
           I have been modeling for the last 3 years. During this time,
           <br className="lg:block hidden" /> I took part in filming in different
           directions and worked with clothing brands.
@@ -22,7 +22,7 @@ export default function About() {
       <img
         src="https://media.discordapp.net/attachments/1054797763348541583/1062373197200244736/image7.jpeg"
         alt="karim katrib"
-        className="sm:h-[450px] sm:w-fit sm:p-8"
+        className="md:h-[450px] md:w-fit md:p-8"
       />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="w-full sm:flex-row flex-col bg-blackish items-center flex justify-center sm:p-2 p-8">
+    <div className="w-full md:flex-row flex-col bg-blackish items-center flex justify-center sm:p-2 p-8">
       <img
         src="https://media.discordapp.net/attachments/1054797763348541583/1062373196805971999/image8.jpeg?width=671&height=671"
         alt="karim katrib"
         className="sm:h-[500px] sm:w-fit"
       />
-      <div className="flex flex-col text-whitish justify-center px-7 sm:pt-0 pt-3 sm:text-start text-center">
-        <p className="font-extrabold sm:text-2xl text-xl ">Karim Katrib</p>
-        <p className="sm:text-xl text-base">Professional Fashion Model</p>
+      <div className="flex flex-col text-whitish justify-center px-7 sm:pt-0 pt-3 md:text-start text-center">
+        <p className="font-extrabold md:text-4xl text-3xl ">Karim Katrib</p>
+        <p className="md:text-2xl text-xl">Professional Fashion Model</p>
       </div>
     </div>
   );

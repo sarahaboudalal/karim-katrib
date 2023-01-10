03143b1e3a217a6f0f38fd556f50929e0ae4fa3e
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 19:59:42 +0200

    end result

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div className="bg-whitish p-10 text-center">
-      <p className="md:text-2xl text-xl text-semibold italic">
-        Let's get in Touch{' '}
-        <a href="tel:+971 76 571 081" className="underline">
-          +971 76 571 081
-        </a>
+    <div className="bg-blackish p-10 text-center text-whitish">
+      <p className="md:text-2xl text-lg text-semibold italic">
+        Let's get in touch
       </p>
+      <a
+        href="tel:+971 76 571 081"
+        className="underline md:inline md:text-2xl text-lg text-semibold italic"
+      >
+        +971 76 571 081
+      </a>
     </div>
   );
 }

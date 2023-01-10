efdb5899585691fd91109fc6303ff767722d7a94
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 18:29:39 +0200

    responsiveness adjustments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="w-full md:flex-row flex-col bg-beige items-center flex justify-center sm:p-2 p-10">
-      <div className="text-blackish text-justify sm:p-8  p-3 flex flex-col justify-center">
+    <div className="w-full md:flex-row flex-col bg-beige items-center flex justify-center md:p-2 p-10">
+      <div className="text-blackish text-justify md:p-8  p-3 flex flex-col justify-center md:w-96">
         <p className="text-4xl underline font-semibold pb-10">About Me</p>
         <p className="pb-3 md:text-xl text-lg">
           Hello! My name is Karim Katrib and I'm a professional fashion model.
         </p>
         <p p className="pb-3 md:text-xl text-lg">
-          I started as an Instagram model, and with consistency and hard work,
-          <br className="lg:block hidden" /> I made a lot of succeess in my
-          modeling career.
+          I started as an Instagram model, and with consistency and hard work, I
+          made a lot of succeess in my modeling career.
         </p>
         <p className="md:text-xl text-lg">
-          I have been modeling for the last 3 years. During this time,
-          <br className="lg:block hidden" /> I took part in filming in different
-          directions and worked with clothing brands.
+          I have been modeling for the last 3 years. During this time, I took
+          part in filming in different directions and worked with clothing
+          brands.
         </p>
       </div>
       <img

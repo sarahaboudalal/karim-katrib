de9718c9d801413974c89f9892aa944bd0e694b2
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 16:56:04 +0200

    Header: responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="w-screen flex-row bg-blackish flex justify-center">
+    <div className="w-screen sm:flex-row flex-col bg-blackish items-center flex justify-center sm:p-2 p-8">
       <img
-        src="https://media.discordapp.net/attachments/1054797763348541583/1062373197535780944/image6.jpeg"
+        src="https://media.discordapp.net/attachments/1054797763348541583/1062373196805971999/image8.jpeg?width=671&height=671"
         alt="karim katrib"
-        className="h-1/3"
+        className="sm:h-96 sm:w-fit"
       />
-      <div className="flex flex-col text-beige justify-center px-7 text-xl">
-        <p className="font-extrabold">Karim Katrib</p>
-        <p className="">Professional Fashion Model</p>
+      <div className="flex flex-col text-whitish justify-center px-7 sm:text-start text-center">
+        <p className="font-extrabold sm:text-2xl text-xl ">Karim Katrib</p>
+        <p className="sm:text-xl text-base ">Professional Fashion Model</p>
       </div>
     </div>
   );

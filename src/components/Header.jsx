17a9360d3f37984e90479e5e4400333206fce9cb
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-01-10 21:39:17 +0200

    fixed header padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="w-full md:flex-row flex-col bg-blackish items-center flex justify-center md:p-2 p-4">
+    <div className="w-full md:flex-row flex-col bg-blackish items-center flex justify-center md:p-3 px-5 py-10">
       <img
         src="https://media.discordapp.net/attachments/1054797763348541583/1062373196805971999/image8.jpeg?width=671&height=671"
         alt="karim katrib"

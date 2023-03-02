1e799b70fd2ea970068c67238c22aaf428767ab4
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2023-03-02 20:08:09 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Gallery from './components/Gallery/Gallery';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Parameters from './components/Parameters/Parameters';
 import './index.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Header />
       <About />
       <Parameters />
